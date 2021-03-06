0b99f1c1c7c4b4865144751a4cf742c7d2759d3c
diyaa <diyaaine@gmail.com>
2021-03-06 15:43:35 -0500

    Reverse array

--- src/main/java/com/diyamolVarghese_C0787971/ArrayElements.java
+++ src/main/java/com/diyamolVarghese_C0787971/ArrayElements.java
@@ -4,12 +4,16 @@ import jdk.nashorn.internal.scripts.JO;
 
 import javax.swing.JOptionPane;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ArrayElements {
     public static void main(String[] args){
         int[] elements;
         elements=insertElements();
         reverseArray(elements);
+        sortedArray(elements);
+        displayOddEvenNumbers(elements);
+        sumOfElements(elements);
     }
 
     public static int[] insertElements(){
@@ -31,5 +35,46 @@ public class ArrayElements {
             j++;
         }
         JOptionPane.showMessageDialog(null,"Array in reverse order is: "+Arrays.toString(reverseArray));
+    }
+
+    public static void sortedArray(int[] elements){
+        Arrays.sort(elements);
+        JOptionPane.showMessageDialog(null,"Sorted array in ascending order is: "+Arrays.toString(elements));
+        int[] reverseArray=new int[10];
+        int j=0;
+        for(int i=elements.length-1;i>=0;i--)
+        {
+            reverseArray[j]=elements[i];
+            j++;
+        }
+        JOptionPane.showMessageDialog(null,"Sorted array in descending order is: "+Arrays.toString(reverseArray));
+    }
+
+    public static void displayOddEvenNumbers(int[] elements){
+        String oddNumbers="";
+        String evenNumbers="";
+        int oddCount=0,evenCount=0;
+        for(int i=0;i<10;i++)
+        {
+            if((elements[i]%2)==0)
+            {
+                evenNumbers+=Integer.toString(elements[i])+", ";
+            }
+            else
+            {
+                oddNumbers+=Integer.toString(elements[i])+", ";
+            }
+        }
+        JOptionPane.showMessageDialog(null,"Odd numbers in the array is: "+oddNumbers);
+        JOptionPane.showMessageDialog(null,"Even numbers in the array is: "+evenNumbers);
+    }
+
+    public static void sumOfElements(int[] elements){
+        int sum=0;
+        for(int i=0;i<10;i++)
+        {
+            sum+=elements[i];
+        }
+        JOptionPane.showMessageDialog(null,"Sum of numbers: "+sum);
     }
 }
