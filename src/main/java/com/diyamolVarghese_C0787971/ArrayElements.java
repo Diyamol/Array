e9690415eee1aac6726e8e7f5b6dfb36978955d2
diyaa <diyaaine@gmail.com>
2021-03-06 11:31:41 -0500

    Array creation

--- src/main/java/com/diyamolVarghese_C0787971/ArrayElements.java
+++ src/main/java/com/diyamolVarghese_C0787971/ArrayElements.java
@@ -1,7 +1,20 @@
 package com.diyamolVarghese_C0787971;
 
+import javax.swing.JOptionPane;
+import java.util.Arrays;
+
 public class ArrayElements {
     public static void main(String[] args){
-        
+        int[] elements=new int[10];
+        insertElements();
+    }
+
+    public static void insertElements(){
+        int[] elements=new int[10];
+        for(int i=0;i<10;i++)
+        {
+            elements[i]=Integer.parseInt(JOptionPane.showInputDialog("Enter the number: "));
+        }
+        JOptionPane.showMessageDialog(null,"Array is: "+Arrays.toString(elements));
     }
 }
